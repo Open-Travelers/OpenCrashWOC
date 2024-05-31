cbed9d0a52705de67351d53c218eaa231fba6036
OpenTravellers <opentravellers@users.noreply.github.com>
2024-05-31 23:45:26 +0200

    Update loadsave.c

--- code/src/gamecode/loadsave.c
+++ code/src/gamecode/loadsave.c
@@ -98,53 +98,51 @@ void DrawGameSlots(Cursor *cursor)
   return;
 }
 
-
-void DrawGameSlot(Game_s *game,float x,float y,s32 col,float size)
-{
+//NGC MATCH
+void DrawGameSlot(struct game_s *game,float x,float y,s32 col,float size) {
   if (game->empty == NULL) {
     Text3D(game->name,x,(y + 0.1500000059604645f),1.0f,size,size,size,1,col);
     CalculateGamePercentage(game);
     sprintf(tbuf,"%i%%",(u32)game->percent);
-    if (Game.language == 'c') {
+    if (Game.language == 0x63) {
       AddSpacesIntoText(tbuf,1);
     }
     Text3D(tbuf,(x - 0.1500000059604645f),y,1.0f,0.6f,0.6f,0.6f,1,col);
     sprintf(tbuf,"%2i ",(u32)game->crystals);
-    if (Game.language == 'c') {
+    if (Game.language == 0x63) {
       AddSpacesIntoText(tbuf,1);
     }
     Text3D(tbuf,(x + 0.1500000059604645f),y,1.0f,0.6f,0.6f,0.6f,1,col);
     sprintf(tbuf,"%2i ",(u32)game->gems);
-    if (Game.language == 'c') {
+    if (Game.language == 0x63) {
       AddSpacesIntoText(tbuf,1);
     }
-    Text3D(tbuf,(x - 0.125),(y - 0.1500000059604645f),1.0f,0.6f,0.6f,0.6f,1,col);
+    Text3D(tbuf,(x - 0.125f),(y - 0.1500000059604645f),1.0f,0.6f,0.6f,0.6f,1,col);
     sprintf(tbuf,"%i%c",(u32)game->relics,(s32)ObjTab[3].font3d_letter);
-    if (Game.language == 'c') {
+    if (Game.language == 0x63) {
       AddSpacesIntoText(tbuf,1);
     }
     Text3D(tbuf,(x + 0.125f),y - 0.1500000059604645f,1.0f,0.6f,0.6f,0.6f,1,col);
     nurndr_forced_mtl = numtl_white;
     sprintf(tbuf,"  %c",(u32)CLetter[117]);
-    if (Game.language == 'c') {
+    if (Game.language == 0x63) {
       AddSpacesIntoText(tbuf,1);
     }
     Text3D(tbuf,(x + 0.1500000059604645f),y,1.0f,0.6f,0.6f,0.6f,1,col);
     sprintf(tbuf,"  %c",(u32)CLetter[119]);
-    if (Game.language == 'c') {
+    if (Game.language == 0x63) {
       AddSpacesIntoText(tbuf,1);
     }
     Text3D(tbuf,x - 0.125f,y - 0.1500000059604645f,1.0f,0.6f,0.6f,0.6f,1,col);
-    nurndr_forced_mtl = game->empty; //nurndr_forced_mtl = 0; ??
-    sprintf(tbuf,"%02d:%02d %02d.%02d.%02d",(u32)game->hours,(u32)game->mins,(u32)game->month,
-            (u32)game->day,(u32)game->year);
-    if (Game.language == 'c') {
+    nurndr_forced_mtl = NULL;
+    sprintf(tbuf,"%02d:%02d %02d.%02d.%02d",(u32)game->hours,(u32)game->mins,(u32)game->month,(u32)game->day,(u32)game->year);
+    if (Game.language == 0x63) {
       AddSpacesIntoText(tbuf,1);
     }
     Text3D(tbuf,x,(y - 0.300000011920929f),1.0f,0.4f,0.4f,0.4f,1,col);
   }
   else {
-    Text3D(tEMPTY[Game.language],x,(y - 0.01999999955296516f),1.0f,size,size,size,1,col) ;
+    Text3D(tEMPTY[Game.language],x,(y - 0.01999999955296516f),1.0f,size,size,size,1,col);
   }
   DrawPanel3DObject(0x81,x,(y - 0.05000000074505806f),1.01f,SLOTPANELSX,0.125f,
                     SLOTPANELSZ,0xc000,0,0,ObjTab[129].obj.scene,ObjTab[129].obj.special,0);
